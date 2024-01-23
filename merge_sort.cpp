4c4dcc71a700e15602509e9dc5fbdfd060e88518
Arun C <arunabh71@gmail.com>
2024-01-23 14:37:14 -0800

    Update merge_sort.cpp
    smaller

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -8,42 +8,21 @@ void merge_sort(vector<int> &ip)
 {
     if (ip.size() > 1)
     {
-        size_t half = max(size_t(0),ip.size()/2);
-        vector<int> half1(ip.begin(),ip.begin()+half);
+        size_t half = max(size_t(0),ip.size()/2), i1 = 0, i2 = 0;
+        vector<int> half1(ip.begin(),ip.begin()+half), half2(ip.begin()+half,ip.end());
         merge_sort(half1);//recursively call on smaller halves; half1 is sorted here
-
-        vector<int> half2(ip.begin()+half,ip.end());
         merge_sort(half2);//half2 is sorted here
 
         ip.clear();//ip vector cleared here
-        int i1 = 0, i2 = 0;
-        
-        while (i1 < half1.size() || i2 < half2.size())
-        {
-            if (i1 >= half1.size())//half1 complete keep pushing rest of half2
-            {
-                ip.emplace_back(half2[i2]);
-                ++i2;
-            }
-            else if (i2 >= half2.size())//half2 complete keep pushing rest of half1
-            {
-                ip.emplace_back(half1[i1]);
-                ++i1;
-            }
-            else//default case
-            {
-                //increment i1 and i2 to keep pushing elements from half1 & half2 into ip
-                if (half1[i1] < half2[i2])
-                {
-                  ip.emplace_back(half1[i1]);
-                  ++i1;
-                }
-                else
-                {
-                  ip.emplace_back(half2[i2]);
 
-                }
-            }
+        while (i1 < half1.size() || i2 < half2.size())
+        {   //half1 complete keep pushing rest of half2
+            if (i1 >= half1.size()) ip.emplace_back(half2[i2++]);
+            //half2 complete keep pushing rest of half1
+            else if (i2 >= half2.size()) ip.emplace_back(half1[i1++]);
+            //default case
+            //increment i1 and i2 to keep pushing elements from half1 & half2 into ip
+            else half1[i1] < half2[i2] ? ip.emplace_back(half1[i1++]) : ip.emplace_back(half2[i2++]);
         }
     }
 }
@@ -59,7 +38,6 @@ int main()
         cout << x<< ", ";
     }
     cout<<endl;
-    cout<<"Hello World";
 
     return 0;
 }
