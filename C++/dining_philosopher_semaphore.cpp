5f21f737b384c43a9635abb55308f36c4738ddca
Arun C <arunabh71@gmail.com>
2024-02-14 15:31:23 -0800

    Update dining_philosopher_semaphore.cpp

--- C++/dining_philosopher_semaphore.cpp
+++ C++/dining_philosopher_semaphore.cpp
@@ -1,30 +1,22 @@
 class Semaphore
 {
-    public:
-    Semaphore() {count=1;} //constructor
-
-    inline void signal()
-    {
-        std::unique_lock<std::mutex> lock(m);
-        if (++count<=0) cv.notify_all();
-    }
-    inline void wait()
-    {
-        std::unique_lock<std::mutex> lock(m);
-        count--;//negative
-        //cout<<"count now "<<count<<endl;
-        cv.wait(lock,[this]{return (count>=0);});//keep waiting until count goes >=0
-    }
-
-    private:
-    std::mutex m;
-    std::condition_variable cv;
     int count;
+    public:
+    Semaphore() {count=1;}//constructor
+    void signal()
+    {
+        ++count;
+    }
+    void wait()
+    {
+        while (count<=0);
+        count--;//negative
+    }
 };
 
 class DiningPhilosophers {
     Semaphore fork[5];
-    std::mutex m;
+    mutex m;
 public:
     DiningPhilosophers() {}
 
@@ -34,19 +26,19 @@ public:
                     function<void()> eat,
                     function<void()> putLeftFork,
                     function<void()> putRightFork) {
-		{
-            std::lock_guard<std::mutex>lock(m);
-            fork[(philosopher+1)%5].wait();
-            fork[philosopher].wait();
-        }
+        
+        lock_guard<mutex>lock(m);
+        
+        fork[philosopher].wait();
+        
         pickLeftFork();
         pickRightFork();
-
         eat();
-
         putLeftFork();
-        fork[(philosopher+1)%5].signal();
         putRightFork();
+
         fork[philosopher].signal();
     }
 };
+
+//Ref: https://leetcode.com/problems/the-dining-philosophers/
