459d292fe39af743237ce2a1801e431d74a3e9da
Arun C <arunabh71@gmail.com>
2024-03-27 16:11:33 -0700

    Update device_file.h

--- device_drivers/SimpleLinuxDriver/device_file.h
+++ device_drivers/SimpleLinuxDriver/device_file.h
@@ -2,6 +2,11 @@
 #define DEVICE_FILE_H_
 #include <linux/compiler.h> /* __must_check */
 
+//__must_check is defined as: #define __must_check __attribute__((warn_unused_result))
+//The warn_unused_result attribute causes a warning to be emitted if a caller of the 
+//function with this attribute does not use its return value. This is useful for 
+//functions where not checking the result is either a security problem or always a bug,
+//such as realloc.
 __must_check int register_device(void); /* 0 if Ok*/
 void unregister_device(void);
 
