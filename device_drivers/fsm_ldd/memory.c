702eee3dae8009ad8b15a5e111c5a209a517ee0a
Arun C <arunabh71@gmail.com>
2024-04-07 01:08:16 -0700

    Update memory.c

--- device_drivers/fsm_ldd/memory.c
+++ device_drivers/fsm_ldd/memory.c
@@ -2,32 +2,11 @@
 
 #include <linux/init.h>
 
-#include <linux/version.h>
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,15)
-#include <linux/config.h>
-#endif
-
 #include <linux/module.h>
-
-#include <linux/kernel.h> /* printk() */
 
 #include <linux/slab.h> /* kmalloc() */
 
-#include <linux/fs.h> /* everything... */
-
-#include <linux/errno.h> /* error codes */
-
-#include <linux/types.h> /* size_t */
-
 #include <linux/proc_fs.h>
-
-#include <linux/fcntl.h> /* O_ACCMODE */
-
-//#include <asm/system.h> /* cli(), *_flags */
-
-#include <asm/uaccess.h> /* copy_from/to_user */
-
-
 
 MODULE_LICENSE("Dual BSD/GPL");
 
