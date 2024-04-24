ecaf2adb35db6c3116bdc6f569e81b6062c6ec11
Arun C <arunabh71@gmail.com>
2024-04-24 15:38:53 -0700

    Update classes.cpp

--- C++/OOPS/classes.cpp
+++ C++/OOPS/classes.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+ 
+// Declare Base Class
+class Base {
+public:
+    int a;
+};
+ 
+// Declare Derived Class
+class Derived : public Base {
+public:
+    int b;
+};
+ 
+// Driver Code
+int main()
+{
+    // Initialise a Derived class geeks
+    Derived geeks;
+ 
+    // Assign value to Derived class variable
+    geeks.b = 3;
+ 
+    // Assign value to Base class variable
+    // via derived class
+    geeks.a = 4;
+ 
+    cout << "Value from derived class: "
+         << geeks.b << endl;
+ 
+    cout << "Value from base class: "
+         << geeks.a << endl;
+ 
+    return 0;
+}
 
+Reference: https://www.geeksforgeeks.org/difference-between-base-class-and-derived-class-in-c/
