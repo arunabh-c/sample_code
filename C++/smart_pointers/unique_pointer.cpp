fc87ca280261a47be33f294cbf642ebca321fd2f
Arun C <arunabh71@gmail.com>
2024-04-23 18:08:32 -0700

    Update unique_pointer.cpp

--- C++/smart_pointers/unique_pointer.cpp
+++ C++/smart_pointers/unique_pointer.cpp
@@ -13,6 +13,17 @@ int main()
     p1->printA(); 
   
     // displays address of the containing pointer 
+    cout << p1.get() << endl;
+
+  /*will give compile time error 
+    unique_ptr<A> p2 = p1; 
+    p2->printA()*/
+
+  // now address stored in p1 shpould get copied to p2 
+    unique_ptr<A> p2 = move(p1); 
+  
+    p2->printA(); 
     cout << p1.get() << endl; 
+    cout << p2.get() << endl; 
     return 0; 
 }
