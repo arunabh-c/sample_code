b71ef9e381c3a00cfdbfd1dea808aa79978f78b0
Arun C <arunabh71@gmail.com>
2024-05-06 21:13:32 -0700

    Update tsla_int.cpp

--- C++/tsla_int.cpp
+++ C++/tsla_int.cpp
@@ -1,60 +1,105 @@
 // std::function <void ()>
 
+#include <bits/stdc++.h>
+using namespace std;
+
 class Work_Queue {
 
-mutex lock;
-condition_variable cv;
-int thrd_ctr, max_thrd;
-queue<pair<int, int>> wq;
-map<int, int> am;
+    mutex lock, qlock, mlock;
+    condition_variable cv, cvq, cvm;
+    int thrd_ctr, max_thrd;
+    queue<pair<int, int>> wq;
+    map<int, int> am;
 
 public:
-Work_Queue(int cap) {
-    max_thrd = cap;
-    thrd_ctr = 0;
-}
+    Work_Queue(int cap) {
+        max_thrd = cap;
+        thrd_ctr = 0;
+    }
+    
+    void enqueue(int ip, int id)
+    {
+        unique_lock<mutex> ul(qlock);
+        wq.push({ ip, id });
+    }
 
-void churn() {
-    if (!wq.empty()) {
-        unique_lock<mutex> ul(lock);
-        cv.wait(ul, [this]() { return (thrd_ctr < max_thrd); });
-        auto [ip, id] = wq.front();
+    void dequeue(int &ip, int &id)
+    {
+        unique_lock<mutex> ul(qlock);
+        cvq.wait(ul, [this]() {return !wq.empty();});
+        ip = wq.front().first;
+        id = wq.front().second;
         wq.pop();
-        ++thrd_ctr;
-        cout << "thrd # " << thrd_ctr << " started" << endl;
-        ip++;
-        sleep(1);
-        cout << "thrd # " << thrd_ctr << " ended" << endl;
+        cvq.notify_all();
+    }
+    
+    void set_ans(int ip, int id)
+    {
+        unique_lock<mutex> ul(mlock);
         am[id] = ip;
-        --thrd_ctr;
-        cv.notify_all();
     }
+    
+    int get_ans(const int id)
+    {
+        int ans;
+        unique_lock<mutex> ul(mlock);
+        cvm.wait(ul, [this, &id]() {return (am.find(id) != am.end());});
+        ans = am[id];
+        cvm.notify_all();
+        return ans;
+    }
+    
+    void new_thread(int &lt)
+    {
+            unique_lock<mutex> ul(lock);
+            cv.wait(ul, [this]() { return (thrd_ctr < max_thrd); });
+            lt = ++thrd_ctr;
+            cv.notify_all();
+    }
+    
+    void end_thread()
+    {
+            unique_lock<mutex> ul(lock);
+            --thrd_ctr;
+            cv.notify_all();
+    }
+    
+    void churn() {
+        
+        
+            int ip, id, lt;
+            dequeue(ip,id);
+
+            new_thread(lt);
+            
+            cout << "thrd # " << lt << " started" << endl;
+            ip++;
+            sleep((rand())%4);
+            cout << "thrd # " << lt << " ended" << endl;
+            
+            
+            end_thread();
 }
 
-void do_work(int ip, int id) {
-    wq.push({ip, id});
-    thread thrdobj([&]() { churn(); });
-    thrdobj.detach();
-}
-
-int get_ans(int id) {
-    while (am.find(id) == am.end()) {
-        // do nothing
+    void do_work(int ip, int id) {
+        enqueue(ip, id);
+        thread thrdobj([&]() { churn(); });
+        thrdobj.detach();
     }
-    return am[id];
-}
 };
 
 int main() {
-cout << "Number of cores: " << thread::hardware_concurrency() << endl;
-Work_Queue q(4);
-for (int i = 0; i < 6; ++i) {
-    q.do_work(i, i + 5);
-}
-for (int i = 0; i < 6; ++i) {
-    cout << q.get_ans(i + 5) << endl;
-}
-return 0;
+
+    Work_Queue q(5);
+    
+    for (int i = 0; i < 15; ++i) {
+        q.do_work(i, i + 5);
+    }
+    
+    for (int i = 0; i < 15; ++i) {
+        cout << q.get_ans(i + 5) << endl;
+    }
+    return 0;
 }
 //width limited work queue
 //constraint - width o the queue - how many jobs concurrently at the same time
