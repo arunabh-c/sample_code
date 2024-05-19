44838ce148de3ad807df318549e3c0ffd6534f7a
Arun C <arunabh71@gmail.com>
2024-05-18 19:22:54 -0700

    Update aligned_free.c

--- aligned_free.c
+++ aligned_free.c
@@ -9,10 +9,11 @@ void* aligned_malloc(size_t size, size_t allignment)
     {
        return NULL;
     }*/
-    //X - X mod A = INT(X / A) * A = X & ~(A - 1)
-    void** p2 = (void**)(((size_t)p1 + offset) & ~(allignment-1));//makes sure starting 
-    //address is multiple of allignment to understand better, remember size will be much
-    //greater than allignment
+    //X - X mod A = INT(X / A) * A = X & ~(A - 1)//doing the remainder function instead of boolean op
+    // boolean op only applies to alignment that are powers of 2
+    void** p2 = (void**)(((size_t)p1 + offset) - ((size_t)p1 + offset)%allignment);//makes sure starting 
+    //makes sure starting address is multiple of allignment to understand better, remember size will 
+    //be much greater than allignment
     p2[-1] = p1;//store original malloc start address to be fed to aligned_free
     
     return p2;
@@ -32,3 +33,4 @@ int main()
     printf("x adress is %p\n",x);
     return 0;
 }
+//Ref: https://stackoverflow.com/questions/38088732/explanation-to-aligned-malloc-implementation
