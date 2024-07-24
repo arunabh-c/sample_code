20d06d41032b752de552c1da751e6dd81593b6f7
Arun C <arunabh71@gmail.com>
2024-07-24 08:54:41 -0700

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -13,27 +13,24 @@ void merge_sort(vector<int> &ip)
         ip.clear();//ip vector cleared here
 
         while (i1 < half1.size() || i2 < half2.size())
-        {   //half1 complete keep pushing rest of half2
-            if (i1 >= half1.size()) ip.emplace_back(half2[i2++]);
+            //half1 complete keep pushing rest of half2
+            if (i1 == half1.size()) ip.emplace_back(half2[i2++]);
             //half2 complete keep pushing rest of half1
-            else if (i2 >= half2.size()) ip.emplace_back(half1[i1++]);
+            else if (i2 == half2.size()) ip.emplace_back(half1[i1++]);
             //default case
             //increment i1 and i2 to keep pushing elements from half1 & half2 into ip
-            else half1[i1] < half2[i2] ? ip.emplace_back(half1[i1++]) : ip.emplace_back(half2[i2++]);
-        }
+            else half1[i1] <= half2[i2] ? ip.emplace_back(half1[i1++]) : ip.emplace_back(half2[i2++]);
     }
 }
 
 int main()
 {
-    vector<int> ip {1,9,8,7,6,5,4,4,3,2,1,0,-5,3};
+    vector<int> ip {-1,-1,-1,-500,1,6,7,0,12};
     merge_sort(ip);
 
     cout<<"ip: ";
     for (auto x: ip)
-    {
         cout << x<< ", ";
-    }
     cout<<endl;
     return 0;
 }
