353d52fab70c0e23d59c36b614b221392b8a243f
Arun C <arunabh71@gmail.com>
2025-08-24 17:27:05 -0700

    Update aligned_free.c
    add explainer for double pointer

--- aligned_free.c
+++ aligned_free.c
@@ -11,7 +11,7 @@ void* aligned_malloc(size_t size, size_t allignment)
     }*/
     //X - X mod A = INT(X / A) * A = X & ~(A - 1)//doing the remainder function instead of boolean op
     // boolean op only applies to alignment that are powers of 2
-    void** p2 = (void**)(((size_t)p1 + offset) - ((size_t)p1 + offset)%allignment);//makes sure starting 
+    void** p2 = (void**)(((size_t)p1 + offset) - ((size_t)p1 + offset)%allignment);//need double pointer for line 17: p2[-1] = p1; 
     //makes sure starting address is multiple of allignment to understand better, remember size will 
     //be much greater than allignment
     p2[-1] = p1;//store original malloc start address to be fed to aligned_free
